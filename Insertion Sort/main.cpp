d7183c5dc4264d31829e513338f2ca29d2cbd8dc
Juan Carlos Conde <jconde001@gmail.com>
2026-04-21 18:36:58 +0200

    Implementado algoritmo

--- Insertion Sort/main.cpp
+++ Insertion Sort/main.cpp
@@ -6,9 +6,29 @@
 //
 
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return EXIT_SUCCESS;
+void insertionSort(vector<int>& arr){
+    int n = arr.size();
+    for(int i=1;i<n;i++){
+        int key = arr[i];
+        cout << "Key: " << key << endl;
+        int j = i-1;
+        cout << "j: " << j << endl;
+        
+        while(j>=0 && arr[j] > key){
+            arr[j+1] = arr[j];
+            j--;
+        }
+        arr[j+1] = key;
+    }
+    
+}
+
+int main() {
+    vector<int> arr = {4,2,5,1,0,6,9,8};
+    
+    insertionSort(arr);
+    
+    for(int x: arr) cout <<" "<< x << ",";
 }
