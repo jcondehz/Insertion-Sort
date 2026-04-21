24feb09556133e8fff11ae9ffdc7962d4cb24d5c
Juan Carlos Conde <jconde001@gmail.com>
2026-04-21 18:25:54 +0200

    Initial Commit

---
+++ Insertion Sort/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Insertion Sort
+//
+//  Created by Juan Carlos Conde Hernández on 21/04/2026.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
